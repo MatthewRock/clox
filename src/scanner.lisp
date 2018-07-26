f4269c45f4e3e465236522d13f271f7e3377fb91
MatthewRock <gwmaniak@gmail.com>
2018-07-26 13:40:25 +0200

    Refactor scanner, add question-mark and colon tokens

--- src/scanner.lisp
+++ src/scanner.lisp
@@ -4,61 +4,85 @@
 ;; Defining Token
 ;; ;;;;;;;;;;;;;;
 
-(defparameter +1char-token-associations+
-  '((#\( :left-paren)
-    (#\) :right-paren)
-    (#\{ :left-brace)
-    (#\} :right-brace)
-    (#\, :comma)
-    (#\. :dot)
-    (#\- :minus)
-    (#\+ :plus)
-    (#\* :star)
-    (#\/ :slash)
-    (#\; :semicolon)))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defparameter +1char-token-associations+
+    ;; Format:
+    ;; List of 2-element lists, where the first element is a character
+    ;; and second element is the token name.
+    ;; ((char token-name) ...)
+    ;; A character is a character and a token name is a keyword symbol.
+    '((#\( :left-paren)
+      (#\) :right-paren)
+      (#\{ :left-brace)
+      (#\} :right-brace)
+      (#\, :comma)
+      (#\. :dot)
+      (#\- :minus)
+      (#\+ :plus)
+      (#\* :star)
+      (#\/ :slash)
+      (#\? :question-mark)
+      (#\: :colon)
+      (#\; :semicolon)))
 
-(defparameter +keywords+
-  #.(let ((keywords-alist
-            '(("and" . :and)
-              ("class" . :class)
-              ("else" . :else)
-              ("false" . :false)
-              ("for" . :for)
-              ("fun" . :fun)
-              ("if" . :if)
-              ("nil" . :nil)
-              ("or" . :or)
-              ("print" . :print)
-              ("return" . :return)
-              ("super" . :super)
-              ("this" . :this)
-              ("true" . :true)
-              ("var" . :var)
-              ("while" . :while))))
-      (alexandria:alist-hash-table keywords-alist
-                                   :test #'equal
-                                   :size (length keywords-alist))))
+  (defparameter +1-or-2-char-token-associations+
+    ;; Format:
+    ;; List of 2 2-element lists, first list being indicating the first character and token name for single character token,
+    ;; Second list having the second character and indicating the 2-character token name:
+    ;; ((first-char 1char-token-name) (second-char 2char-token-name) ...)
+    ;; A character is a character and token name is a keyword symbol
+    '(((#\! :bang) (#\= :bang-equal))
+      ((#\= :equal) (#\= :equal-equal))
+      ((#\> :greater) (#\= :greater-equal))
+      ((#\< :less) (#\= :less-equal))))
+
+  (defparameter +literals+
+    ;; Format:
+    ;; List of token names
+    ;; (token1 token2 ...)
+    ;; Token names are keyword symbols.
+    '(:identifier :string :number))
+
+  (defparameter +keywords-alist+
+    ;; Format: association list of lexeme(token) and token name pairs (dotted lists)
+    ;; ((lexeme . token-name) ...)
+    ;; Lexeme is a string, token name is a keyword symbol.
+    '(("and" . :and)
+      ("class" . :class)
+      ("else" . :else)
+      ("false" . :false)
+      ("for" . :for)
+      ("fun" . :fun)
+      ("if" . :if)
+      ("nil" . :nil)
+      ("or" . :or)
+      ("print" . :print)
+      ("return" . :return)
+      ("super" . :super)
+      ("this" . :this)
+      ("true" . :true)
+      ("var" . :var)
+      ("while" . :while)))
+
+  (defparameter +keywords+
+    ;; A hash table created from keyword-alist.
+    ;; Key being the lexeme and value being the token name.
+    ;; Lexeme is a string and token name is a keyword symbol.
+    (alexandria:alist-hash-table +keywords-alist+
+                                 :test #'equal
+                                 :size (length +keywords-alist+))))
 
 (deftype token-type ()
   `(member
     ;; Single-character tokens
-    :left-paren :right-paren
-    :left-brace :right-brace
-    :comma :dot :minus :plus :star :slash :semicolon
-
+    ,@(mapcar #'second +1char-token-associations+)
     ;; One or two character tokens
-                :bang :bang-equal
-                :equal :equal-equal
-                :greater :greater-equal
-                :less :less-equal
-
-                ;; Literals
-                :identifier :string :number
-
-                ;; Keywords.
-    :and :class :else :false :fun :for :if :nil :or
-                :print :return :super :this :true :var :while
-
+    ,@(mapcan (alexandria:curry #'mapcar #'second) +1-or-2-char-token-associations+)
+    ;; Literals
+    ,@+literals+
+    ;; Keywords.
+    ,@(mapcar #'cdr +keywords-alist+)
+    ;; EOF token
     :eof))
 
 (defclass Token ()
@@ -156,6 +180,7 @@
 (defmethod is-at-end ((scanner Scanner))
   "Return T if scanner reached EOF, nil otherwise."
   (>= (current scanner) (length (source scanner))))
+
 (-> advance (scanner) character)
 (defun advance (scanner)
   "Advance scanner's pointer and return the character at that point."
@@ -164,79 +189,54 @@
 (-> match (scanner character) boolean)
 (defun match (scanner expected)
   "Consume the next character and return T if it is expected character, return NIL otherwise."
-  (unless (or (is-at-end scanner)
-              (char/= (current-character scanner) expected))
+  (unless (char/= (peek scanner) expected)
     (move-forward scanner)
     t))
 
 (-> peek (scanner) character)
 (defun peek (scanner)
-  "Return the next character in the source or NUL if we're at the end."
+  "Return the next character in the source or End-of-text token if we're at the end."
   (if (is-at-end scanner)
-      #\Nul
-      (current-character scanner)))
+      #\Etx
+      (aref (source scanner) (current scanner))))
 
 (-> peek-next (scanner) character)
 (defun peek-next (scanner)
-  "Return the character after the next character in the source or NUL if there's none.'"
+  "Return the character after the next character in the source or ETX if there's none.'"
   (if (>= (1+ (current scanner))
           (length (source scanner)))
-      #\Nul
+      #\Etx
       (aref (source scanner)
             (1+ (current scanner)))))
 
-(-> scan-token (scanner) null)
+(-> current-character (scanner) character)
+(defun current-character (scanner)
+  "Return the current character of the source - the one that is processed right now, or STX if processing hasn't started yet."
+  (if (zerop (current scanner))
+      #\Stx
+      (aref (source scanner) (1- (current scanner)))))
+
+(defun is-at-newline (scanner)
+  "Return T if scanner is at newline(next character will be \n)."
+  (char= (peek scanner) #\Linefeed))
+
+(-> scan-token (scanner) (or boolean token))
 (defun scan-token (scanner)
   "Add next token from the scanner's source."
-  (let ((currently-processed-character (advance scanner)))
-    (case currently-processed-character
-      ;; One-character tokens
-      (#\( (add-token scanner :left-paren))
-      (#\) (add-token scanner :right-paren))
-      (#\{ (add-token scanner :left-brace))
-      (#\} (add-token scanner :right-brace))
-      (#\, (add-token scanner :comma))
-      (#\. (add-token scanner :dot))
-      (#\- (add-token scanner :minus))
-      (#\+ (add-token scanner :plus))
-      (#\; (add-token scanner :semicolon))
-      (#\* (add-token scanner :star))
-
-      ;; One-Or-Two-character tokens
-      (#\! (add-token scanner (if (match scanner #\=)
-                                  :bang-equal
-                                  :bang)))
-      (#\= (add-token scanner (if (match scanner #\=)
-                                  :equal-equal
-                                  :equal)))
-      (#\< (add-token scanner (if (match scanner #\=)
-                                  :less-equal
-                                  :less)))
-      (#\> (add-token scanner (if (match scanner #\=)
-                                  :greater-equal
-                                  :greater)))
-
-      ;; Process Comments
-      (#\/ (if (match scanner #\/)
-               (process-single-line-comment scanner)
-               (add-token scanner :slash)))
-
-      ;; Ignore whitespace. Newline is processed automatically when moving position
-      ((#\Space #\Tab #\Return #\Linefeed) nil)
-
-      ;; Strings
-      (#\" (process-string scanner))
-
-      (otherwise
-       (cond
-         ((is-digit currently-processed-character) (process-number-literal scanner))
-         ((is-alpha currently-processed-character) (process-identifier scanner))
-         (t (cerror "Continue scanning."
-                    'unexpected-character-error
-                    :character currently-processed-character
-                    :line (line scanner)
-                    :place (start scanner)))))))
-  nil)
+  ;; FIXME: Make cerror return something else than NIL.
+  (advance scanner)
+  (or (process-1char-token scanner)
+      (process-1-or-2-char-token scanner)
+      (process-comment-or-slash scanner)
+      (process-whitespace scanner)
+      (process-string scanner)
+      (process-number-literal scanner)
+      (process-identifier scanner)
+      (cerror "Continue scanning."
+              'unexpected-character-error
+              :character currently-processed-character
+              :line (line scanner)
+              :place (start scanner))))
 
 (-> scan-tokens (string) vector)
 (defun scan-tokens (input)
@@ -251,36 +251,64 @@
                      (add-token scanner :eof)
                      (tokens scanner)))))
 
-(-> add-token (scanner token-type &optional t) null)
+(-> add-token (scanner token-type &optional t) token)
 (defun add-token (scanner token-type &optional literal)
   "Add token to the vector of scanned tokens."
-  (vector-push-extend
-   (make-instance'token :type token-type
-                        :lexeme (subseq (source scanner)
-                                        (start scanner)
-                                        (current scanner))
-                        :literal literal
-                        :line (line scanner))
-   (tokens scanner))
-  nil)
+  (let ((token (make-instance'token :type token-type
+                                    :lexeme (subseq (source scanner)
+                                                    (start scanner)
+                                                    (current scanner))
+                                    :literal literal
+                                    :line (line scanner))))
+    (vector-push-extend token (tokens scanner))
+    token))
 
-(-> current-character (scanner) character)
-(defun current-character (scanner)
-  "Return the current character of the source - the one that will be processed now."
-  (aref (source scanner) (current scanner)))
 
-(defun is-at-newline (scanner)
-  "Return T if scanner is at newline(next character will be \n)."
-  (char= (peek scanner) #\Linefeed))
+(-> process-1char-token (scanner) (or null token))
+(defun process-1char-token (scanner)
+  (loop
+    ;; Potential optimization: create a case form
+    ;; at a read-time to save looping
+    with currently-processed-character = (current-character scanner)
+    for (potential-char potential-token) in +1char-token-associations+
+    if (char= potential-char currently-processed-character) return (add-token scanner potential-token)))
 
-(-> process-single-line-comment (scanner) null)
+(-> process-1-or-2-char-token (scanner) (or null token))
+(defun process-1-or-2-char-token (scanner)
+  (loop
+    ;; Potential optimization: create a case form
+    ;; at a read-time to save looping
+    with currently-processed-character = (current-character scanner)
+    for ((first-char 1-token-name)
+         (second-char 2-token-name))
+      in +1-or-2-char-token-associations+
+    if (char= currently-processed-character first-char)
+      if (match scanner second-char)
+        return (add-token scanner 2-token-name)
+    ;; this else is for the second if, not the first one
+    else return (add-token scanner 1-token-name)))
+
+(-> process-comment-or-slash (scanner) (or null token))
+(defun process-comment-or-slash (scanner)
+  (if (char= (current-character scanner) #\/)
+      (if (match scanner #\/)
+          (process-single-line-comment scanner)
+          (add-token scanner :slash))))
+
+(-> process-whitespace (scanner) boolean)
+(defun process-whitespace (scanner)
+  (some (alexandria:curry #'char= (current-character scanner))
+        (list #\Space #\Tab #\Return #\Linefeed)))
+
+(-> process-single-line-comment (scanner) boolean)
 (defun process-single-line-comment (scanner)
   "Consume contents of until EOL or EOF."
   (loop until (or (is-at-newline scanner)
                   (is-at-end scanner))
-        do (advance scanner)))
+        do (advance scanner))
+  t)
 
-(-> process-string (scanner) null)
+(-> process-string (scanner) (or null token))
 (defun process-string (scanner)
   "Consume the string from the current point to the closing double quote."
   (flet ((add-string (scanner)
@@ -289,20 +317,22 @@
                               (1+ (start scanner))
                               (1- (current scanner))))))
 
-    (loop until (or (char= (peek scanner) #\")
-                    (is-at-end scanner))
-          do (advance scanner))
+    ;; Only process the string if it starts as a string
+    (when (char= (current-character scanner) #\")
+      (loop until (or (char= (peek scanner) #\")
+                      (is-at-end scanner))
+            do (advance scanner))
 
-    ;; If String's not terminated
-    (if (is-at-end scanner)
-        ;; Report it
-        (cerror "Continue scanning"
-                'unterminated-string-error
-                :line (line scanner)
-                :place (start scanner))
-        ;; Consume the closing "
-        (advance scanner))
-    (add-string scanner)))
+      ;; If String's not terminated
+      (if (is-at-end scanner)
+          ;; Report it
+          (cerror "Continue scanning"
+                  'unterminated-string-error
+                  :line (line scanner)
+                  :place (start scanner))
+          ;; Otherwise consume the closing "
+          (advance scanner))
+      (add-string scanner))))
 
 (-> is-digit (character) boolean)
 (defun is-digit (char)
@@ -324,25 +354,34 @@
   (or (is-alpha char)
       (is-digit char)))
 
-(-> process-number-literal (scanner) null)
+;; TODO: Add .123 format
+(-> process-number-literal (scanner) (or token null))
 (defun process-number-literal (scanner)
   "Process a number literal and return T, or return NIL and don't process anything if the token isn't number literal."
-  (loop while (is-digit (peek scanner)) do (advance scanner))
-  (when (and (char= (peek scanner) #\.)
-             (is-digit (peek-next scanner)))
-    ;; Consume the dot
-    (advance scanner)
-    (loop while (is-digit (peek scanner)) do (advance scanner)))
-  (add-token scanner :number (read-from-string
-                              (subseq (source scanner)
-                                      (start scanner)
-                                      (current scanner)))))
+  (when (or (is-digit (current-character scanner)))
 
-(-> process-identifier (scanner) null)
+    (loop while (is-digit (peek scanner)) do (advance scanner))
+
+    ;; Check if the number is a floating point one
+    (when (and (char= (peek scanner) #\.)
+               (is-digit (peek-next scanner)))
+      ;; Consume the dot
+      (advance scanner)
+      ;; And collect the rest
+      (loop while (is-digit (peek scanner)) do (advance scanner)))
+
+    (add-token scanner :number (read-from-string
+                                (subseq (source scanner)
+                                        (start scanner)
+                                        (current scanner))))))
+
+(-> process-identifier (scanner) (or token null))
 (defun process-identifier (scanner)
-  (loop while (is-alphanumeric (peek scanner)) do (advance scanner))
-  (add-token scanner
-             (gethash
-              (subseq (source scanner) (start scanner) (current scanner))
-              +keywords+
-              :identifier)))
+  "Return TOKEN if it is an identifier, NIL otherwise."
+  (when (is-alpha (current-character scanner)) ; valid identifier starts with an alphabet character.
+    (loop while (is-alphanumeric (peek scanner)) do (advance scanner))
+    (add-token scanner
+               (gethash
+                (subseq (source scanner) (start scanner) (current scanner))
+                +keywords+
+                :identifier))))
