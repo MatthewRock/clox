1fd8d0e62d194f276aaf822f36571ce9260614a7
MatthewRock <maliszmat@gmail.com>
2018-09-15 01:38:21 +0200

    Define ast for statement, add comments, indent.

--- src/ast.lisp
+++ src/ast.lisp
@@ -1,9 +1,15 @@
 (in-package :clox)
 
+;; Abbreviation for the "Expression"
 (defast Expr (:naming-convention :lisp-postfix)
-        (Ternary -> (question Expr) (operator Token) (result-true Expr) (separator Token) (result-false Expr))
-        (Binary -> (left Expr) (operator Token) (right Expr))
-        (Broken-Binary -> (operator Token) (right Expr))
-        (Grouping -> (expression Expr))
-        (Literal -> value)
-        (Unary -> (operator Token) (right Expr)))
+  (Ternary -> (question Expr) (operator Token) (result-true Expr) (separator Token) (result-false Expr))
+  (Binary -> (left Expr) (operator Token) (right Expr))
+  (Broken-Binary -> (operator Token) (right Expr))
+  (Grouping -> (expression Expr))
+  (Literal -> value)
+  (Unary -> (operator Token) (right Expr)))
+
+;; Abbreviation for a "statement"
+(defast stmt (:naming-convention :lisp-postfix)
+  (Expression -> (expression Expr))
+  (Print -> (expression Expr)))
